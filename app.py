d93b4956109361112512127576da2dfa328f0eb2
interigence <yjh56781@gmail.com>
2025-01-31 19:59:23 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
-from burn_tracker import get_total_burned
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/api/burned", methods=["GET"])
-def burned_tokens():
-    """총 소각량을 반환하는 API"""
-    return jsonify({"total_burned": get_total_burned()})
+@app.route('/')
+def home():
+    return render_template('index.html')  # templates/index.html을 렌더링
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
